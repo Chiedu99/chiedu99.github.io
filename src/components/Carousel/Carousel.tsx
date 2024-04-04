febd8bdd4317365d9f352d4e0a2d0f8b06f6fe8e
cninfinity <cninfinity8@gmail.com>
2024-04-04 14:03:15 -0500

    mobile changes

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -10,7 +10,7 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ images }) => {
   const [centerIndex, setCenterIndex] = useState(0);
-  const slidesToShow = 3;
+  let slidesToShow = 3;
 
   const settings = {
     dots: false,
@@ -22,14 +22,20 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
     autoplaySpeed: 2500, // Ensure this matches the CSS animation timing
     cssEase: "linear",
     beforeChange: (current: number, next: number) => {
-      const newIndex = (next + Math.floor(slidesToShow / 2)) % images.length;
-      setCenterIndex(newIndex);
+      // Adjust centerIndex based on slidesToShow
+      if (settings.slidesToShow === 1) {
+        setCenterIndex(next); // On mobile, the next slide becomes the center by default
+      } else {
+        const newIndex = (next + Math.floor(slidesToShow / 2)) % images.length;
+        setCenterIndex(newIndex);
+      }
     },
     responsive: [
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
+          // Additional responsive settings can be added here if necessary
         },
       },
     ],
@@ -39,6 +45,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
     <div className={styles.carousel}>
       <Slider {...settings}>
         {images.map((img, idx) => (
+          // Apply the centerSlide class if it's the only slide shown or it's the center in a multi-slide view
           <div key={idx} className={idx === centerIndex ? styles.centerSlide : styles.slide}>
             <img src={img} alt={`Slide ${idx}`} className={styles.image} />
           </div>
